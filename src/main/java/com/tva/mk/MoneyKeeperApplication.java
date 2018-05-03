79fc6d66f2a1a9247e4922417b5c1e6a4772d257
Zng Tfy <nvt87x@gmail.com>
2018-05-03 15:37:10 +0700

    Add template project

---
+++ src/main/java/com/tva/mk/MoneyKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.tva.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyKeeperApplication.class, args);
+	}
+}
