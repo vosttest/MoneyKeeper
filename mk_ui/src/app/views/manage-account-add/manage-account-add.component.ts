640f6fb54f9dddaa11c27253adbc1f51eedfd8c6
Lê Bá Công <lebacong1409it@gmail.com>
2018-05-09 16:59:25 +0700

    Edit ui manage-account. Integrate api common into angular

--- mk_ui/src/app/views/manage-account-add/manage-account-add.component.ts
+++ mk_ui/src/app/views/manage-account-add/manage-account-add.component.ts
@@ -10,19 +10,41 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 })
 export class ManageAccountAddComponent implements OnInit {
     public lstType: any[] = [];
+    public lstCurrency: any[] = [];
+    public lstTerm: any[] = [];
+    public lstInterestPaid: any[] = [];
+    public lstTermEnd: any[] = [];
     public selected = '';
 
     constructor(private proCommon: CommonProvider) { }
 
     ngOnInit() {
-        this.getType();
+        this.getType('Account');
+        this.getType('Currency');
+        this.getType('Term');
+        this.getType('InterestPaid');
+        this.getType('TermEnd');
     }
 
-    private getType() {
-        this.proCommon.search('Account').subscribe((rsp: any) => {
+    private getType(type: string) {
+        this.proCommon.search(type).subscribe((rsp: any) => {
             if (rsp.status === 'success') {
-                this.lstType = rsp.result.data;
-                console.log(this.lstType);
+                if (type == 'Account') {
+                    this.lstType = rsp.result.data;
+                    console.log(this.lstType);
+                } else if (type == 'Currency') {
+                    this.lstCurrency = rsp.result.data;
+                    console.log(this.lstCurrency);
+                } else if (type == 'Term') {
+                    this.lstTerm = rsp.result.data;
+                    console.log(this.lstTerm);
+                } else if (type == 'InterestPaid') {
+                    this.lstInterestPaid = rsp.result.data;
+                    console.log(this.lstInterestPaid);
+                } else if (type == 'TermEnd') {
+                    this.lstTermEnd = rsp.result.data;
+                    console.log(this.lstTermEnd);
+                }
             }
 
         }, err => this.proCommon.handleError(err));
--- mk_ui/src/app/views/manage-account-edit/manage-account-edit.component.ts
+++ mk_ui/src/app/views/manage-account-edit/manage-account-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { Router } from '@angular/router';
+import { CommonProvider } from '../../providers/providers';
 
 @Component({
     selector: 'app-manage-account-edit',
@@ -9,7 +10,44 @@ import { Router } from '@angular/router';
 })
 
 export class ManageAccountEditComponent implements OnInit {
-    constructor() { }
+    public lstType: any[] = [];
+    public lstCurrency: any[] = [];
+    public lstTerm: any[] = [];
+    public lstInterestPaid: any[] = [];
+    public lstTermEnd: any[] = [];
+    public selected = '';
 
-    ngOnInit() { }
+    constructor(private proCommon: CommonProvider) { }
+
+    ngOnInit() {
+        this.getType('Account');
+        this.getType('Currency');
+        this.getType('Term');
+        this.getType('InterestPaid');
+        this.getType('TermEnd');
+    }
+
+    private getType(type: string) {
+        this.proCommon.search(type).subscribe((rsp: any) => {
+            if (rsp.status === 'success') {
+                if (type == 'Account') {
+                    this.lstType = rsp.result.data;
+                    console.log(this.lstType);
+                } else if (type == 'Currency') {
+                    this.lstCurrency = rsp.result.data;
+                    console.log(this.lstCurrency);
+                } else if (type == 'Term') {
+                    this.lstTerm = rsp.result.data;
+                    console.log(this.lstTerm);
+                } else if (type == 'InterestPaid') {
+                    this.lstInterestPaid = rsp.result.data;
+                    console.log(this.lstInterestPaid);
+                } else if (type == 'TermEnd') {
+                    this.lstTermEnd = rsp.result.data;
+                    console.log(this.lstTermEnd);
+                }
+            }
+
+        }, err => this.proCommon.handleError(err));
+    }
 }
