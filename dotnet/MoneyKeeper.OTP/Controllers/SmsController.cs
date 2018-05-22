56907237f4557c9eef76c712652473bceef9b0af
Zng Tfy <nvt87x@gmail.com>
2018-05-22 16:00:07 +0700

    Test SMS OK

---
+++ dotnet/MoneyKeeper.OTP/Controllers/SmsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO.Ports;
+
+namespace MoneyKeeper.OTP.Controllers
+{
+    using Models;
+    using UTL;
+
+    [Produces("application/json")]
+    [Route("api/Sms")]
+    public class SmsController : Controller
+    {
+        SerialPort _port = new SerialPort();
+        SMS _sms = new SMS();
+        ShortMessageCollection _smc = new ShortMessageCollection();
+
+        [HttpPost]
+        public string Post([FromBody]SMSReq req)
+        {
+            var res = "OK";
+            try
+            {
+                _port = _sms.OpenPort(Program._port);
+                _sms.SendSMS(_port, req.Phone, req.Message);
+
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ dotnet/MoneyKeeper.OTP/Models/SMSReq.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyKeeper.OTP.Models
+{
+    public class SMSReq
+    {
+        public string Phone { get; set; }
+
+        public string Message { get; set; }
+    }
+}
