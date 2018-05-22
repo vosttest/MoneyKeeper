b7839a383340edc5e90cae6b752dc2d1e54dfea6
Zng Tfy <nvt87x@gmail.com>
2018-05-22 15:42:42 +0700

    Add SMS server

---
+++ dotnet/MoneyKeeper.OTP/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MoneyKeeper.OTP
+{
+    using UTL;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            _port = SMS.DetectPort();
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+        public static string _port;
+    }
+}
