a4d3bd44eb783dbaf3e5bfee6219dbbce506a1c3
Zng Tfy <nvt87x@gmail.com>
2018-05-23 07:25:49 +0700

    Add API OTP and Token application

---
+++ dotnet/MoneyKeeper.Api/Req/SMSReq.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneyKeeper.Api.Req
+{
+    public class SMSReq
+    {
+        public string Phone { get; set; }
+
+        public string Message { get; set; }
+
+        public string User { get; set; }
+
+        public string Pass { get; set; }
+    }
+}
---
+++ dotnet/MoneyKeeper.Sms/ShortMessage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace MoneyKeeper.Sms
+{
+    /// <summary>
+    /// Short message
+    /// </summary>
+    public class ShortMessage
+    {
+        public string Index { get; set; }
+
+        public string Status { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Alphabet { get; set; }
+
+        public string Sent { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    /// <summary>
+    /// ShortMessage collection
+    /// </summary>
+    public class ShortMessageCollection : List<ShortMessage> { }
+}
