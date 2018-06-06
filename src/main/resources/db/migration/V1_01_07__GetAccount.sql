b33ceaa849079072cb564abef0731b61609852f4
Zng Tfy <nvt87x@gmail.com>
2018-06-06 21:18:48 +0700

    VOST 275

--- src/main/resources/db/migration/V1_01_07__GetAccount.sql
+++ src/main/resources/db/migration/V1_01_07__GetAccount.sql
@@ -6,7 +6,8 @@ CREATE OR REPLACE FUNCTION get_account(keyword VARCHAR, uid INT4)
 RETURNS TABLE(id INT4, code VARCHAR, text VARCHAR, balance FLOAT8, currency VARCHAR, rate FLOAT8) AS $body$
 BEGIN
 RETURN QUERY
-	SELECT a.id, a.type, a.text, a.balance + COALESCE(b.total, 0) as balance, a.currency, f.rate FROM account a
+	SELECT a.id, a.type, a.text, a.balance + COALESCE(b.total, 0) as balance, a.currency, c.rate
+	FROM account a
 	LEFT JOIN	(
 					SELECT account_id, SUM(CASE WHEN type = 'Income' THEN total ELSE -total END) AS total
 					FROM voucher
@@ -14,14 +15,17 @@ RETURN QUERY
 					GROUP BY account_id
 				) b ON a.id = b.account_id
 	LEFT JOIN	(
-					SELECT p.value, (p.text::FLOAT8/(
-								SELECT COALESCE(ch.text, '1')
-								FROM common ch
-								JOIN setting s ON s.text = 'Currency' AND ch.value = COALESCE(s.value, 'VND') AND s.user_id = uid
-								WHERE ch.type = 'Rate')::FLOAT8) AS rate
+					SELECT p.value, (p.text::FLOAT8 /	(
+															SELECT COALESCE(ch.text, '1')
+															FROM common ch
+															JOIN setting s ON s.text = 'Currency'
+																AND ch.value = COALESCE(s.value, 'VND')
+																AND s.user_id = uid
+																WHERE ch.type = 'Rate'
+														)::FLOAT8) AS rate
 					FROM common p
 					WHERE p.type = 'Rate'
-				) f ON f.value = a.currency
+				) c ON c.value = a.currency
 	WHERE a.user_id = uid
 	AND a.is_deleted = FALSE
 	AND UPPER(a.text) LIKE UPPER(CONCAT('%', keyword, '%'));
